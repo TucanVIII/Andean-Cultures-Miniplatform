3280c00fe0d466915fc5c4b941e0962e5141aec1
mesias <tulcan888@gmail.com>
2025-11-28 22:21:35 -0500

    v2.2.4.1 Added icons to the public page and fixed a minor bug in the Dashnav-hero-moreButton caused by z-index

--- frontend/src/components/publicComponents/DashLearnSection.jsx
+++ frontend/src/components/publicComponents/DashLearnSection.jsx
@@ -1,42 +1,48 @@
-import reactLogo from "../../assets/react.svg";
+import Motivador from "../../assets/Motivador.png";
+import Visual from "../../assets/Visual.png";
+import Interactivo from "../../assets/Interactivo.png";
+import Personalizado from "../../assets/Personalizado.png";
+import Multiplataforma from "../../assets/Multiplataforma.png";
+import Alegre from "../../assets/Alegre.png";
+import Expertos from "../../assets/Expertos.png";
+import Reporte from "../../assets/Reporte.png";
 
 const DashSection = () => {
   const content = (
     <section className="section-learning__container" id="infoSection">
-
       <div className="learn-container">
         <h2 className="sub-title">Desarrollando aprendizajes interactivos</h2>
         <div className="learning-types">
           <div className="learn-type">
-            <img src={reactLogo} alt="" className="img-type" />
+            <img src={Motivador} alt="" className="img-type" />
             <h3 className="sub-subtitle">MOTIVADOR</h3>
             <p className="description">
               Capta <b>el interés</b> por aprender
             </p>
           </div>
           <div className="learn-type">
-            <img src={reactLogo} alt="" className="img-type" />
+            <img src={Visual} alt="" className="img-type" />
             <h3 className="sub-subtitle">VISUAL</h3>
             <p className="description">
               Facilita la <b>compresión</b>
             </p>
           </div>
           <div className="learn-type">
-            <img src={reactLogo} alt="" className="img-type" />
+            <img src={Interactivo} alt="" className="img-type" />
             <h3 className="sub-subtitle">INTERACTIVO</h3>
             <p className="description">
               Promoviendo <b>aprendizajes</b> activos
             </p>
           </div>
           <div className="learn-type">
-            <img src={reactLogo} alt="" className="img-type" />
+            <img src={Personalizado} alt="" className="img-type" />
             <h3 className="sub-subtitle">PERSONALIZADO</h3>
             <p className="description">
               Adaptado a los <b>contenidos</b> temarios
             </p>
           </div>
           <div className="learn-type">
-            <img src={reactLogo} alt="" className="img-type" />
+            <img src={Multiplataforma} alt="" className="img-type" />
             <h3 className="sub-subtitle">MULTI PLATAFORMA</h3>
             <p className="description">
               Desarrollado para todos los <b>dispositivos</b>{" "}
@@ -53,33 +59,38 @@ const DashSection = () => {
             por medio de actividades lúdicas e interactivas
           </p>
         </div>
-        <div className="card-study">
-          <div className="card-description">
-            <h3 className="sub-subtitle">Alegre y encantador</h3>
-            <p className="card-study__description">
-              Explora actividades creativas por distintas secciones narrativas para un aprendizaje divertido y estimulante
-            </p>
+        <div className="cards-grid">
+          <div className="card-study">
+            <div className="card-description">
+              <h3 className="sub-subtitle">Alegre y encantador</h3>
+              <p className="card-study__description">
+                Explora actividades creativas por distintas secciones narrativas
+                para un aprendizaje divertido y estimulante
+              </p>
+            </div>
+            <img src={Alegre} alt="" className="img-study" />
           </div>
-          <img src={reactLogo} alt="" className="img-study" />
-        </div>
-        <div className="card-study">
-          <img src={reactLogo} alt="" className="img-study" />
-          <div className="card-description">
-            <h3 className="sub-subtitle">Desarrollado por expertos</h3>
-            <p className="card-study__description">
-              Creado en colaboración de expertos en aprendizaje de historia
-              sudamericana
-            </p>
+          <div className="card-study">
+            <img src={Expertos} alt="" className="img-study" />
+            <div className="card-description">
+              <h3 className="sub-subtitle">Desarrollado por expertos</h3>
+              <p className="card-study__description">
+                Creado en colaboración de expertos en aprendizaje de historia
+                sudamericana
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="card-study">
-          <div className="card-description">
-            <h3 className="sub-subtitle">Reporte de seguimiento personalizado</h3>
-            <p className="card-study__description">
-              Monitorización del progreso del estudiante constante
-            </p>
+          <div className="card-study">
+            <div className="card-description">
+              <h3 className="sub-subtitle">
+                Reporte de seguimiento personalizado
+              </h3>
+              <p className="card-study__description">
+                Monitorización del progreso del estudiante constante
+              </p>
+            </div>
+            <img src={Reporte} alt="" className="img-study" />
           </div>
-          <img src={reactLogo} alt="" className="img-study" />
         </div>
       </div>
     </section>
